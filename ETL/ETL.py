70b7c48ea6862a80f69789c97b0e856fe2a21be7
Rohit Kumar Prajapati <44048670+rohitrsp898@users.noreply.github.com>
2022-01-02 10:51:52 +0530

    Update ETL.py
    Added Comments

--- ETL/ETL.py
+++ ETL/ETL.py
@@ -26,22 +26,22 @@ def extract_from_mysql(query):
     cursor = connection.cursor()
     # Execute the query
     cursor.execute(query)
+    # Fetch all the Column Names
     field_names = [i[0] for i in cursor.description]
-    #
-    # print(field_names)
+    
     # Get the data
     data = cursor.fetchall()
     # Close the connection
     connection.close()
     # Create a dataframe
     df = pd.DataFrame(data, columns=field_names)
-    #df.columns = field_names
+    
     #print(df)
     
     return df
 
 
-def extract_from_csv(path):
+def extract_from_csv(path):         # Extract data from csv file and return it as a dataframe
     """
     Extract data from a csv file and return it as a dataframe
     :param path: Path to the csv file
@@ -53,7 +53,7 @@ def extract_from_csv(path):
     
     return df
 
-def extract_from_json(path):
+def extract_from_json(path):        # Extract data from json file and return it as a dataframe
     """
     Extract data from a json file and return it as a dataframe
     :param path: Path to the json file
@@ -66,45 +66,49 @@ def extract_from_json(path):
     return df
 
 
-def extract():
+def extract():                  # Extract data from MySQL, csv and json files
+
     extracted_data=pd.DataFrame()       # Create a empty dataframe
 
-    #process all csv files
+    # Extract data from CSV files
     for csv in glob.glob(path+"*.csv"):
         extracted_data=extracted_data.append(extract_from_csv(csv),ignore_index=True)
     logging.info("Extracted data from csv files")
     
-
+    # Extract data from JSON files
     for json in glob.glob(path+"*.json"):
         extracted_data=extracted_data.append(extract_from_json(json),ignore_index=True)
     logging.info("Extracted data from json files")
     
+    # Extract data from MySQL database
     extracted_data=extracted_data.append(extract_from_mysql("select * from data"),ignore_index=True)
     logging.info("Extracted data from MySQL table")
 
+    #return the extracted data
     return extracted_data
 
-def transform(data):
+def transform(data):         # Transformation
 
+    # Create a new column with the transformation
     data['New Salary(INR)']=data['Salary(INR)']*1.2
     logging.info("Transformed data: new column added as New Salary(INR)")
-
+    
+    # Create a new column with the transformation
     data['Salary(USD)']=round(data['New Salary(INR)']/75.4,2)
     logging.info("Transformed data: new column added as Salary(USD)")
 
     logging.info("Transformed data !!")
-    return data
+    return data         # Return the transformed data
 
-def load(data):
+def load(data):         # Load data into a csv file
     data.to_csv(path+'output.csv',index=False)
     logging.info("Loaded data into csv file")
 
-
+# Main functions
+# Extract data from MySQL, csv and json files
 extracted_data=extract()
+# Transform the data
 transformed_data=transform(extracted_data)
+# Load the transformed data into a csv file
 load(transformed_data)
-
-
-
-
 
